86a0c31fd7c2c1c4caa72bd4e5f1048c8ccd55aa
Yuri Silva <44654440+WolfgangVing@users.noreply.github.com>
2022-01-18 10:52:41 -0300

    Day 3.1 how powerfull the use of middleware can be

--- authorize.js
+++ authorize.js
@@ -1,4 +1,14 @@
+// !!! THIS IS NOT THE REAL WAY TO AUTHORIZE, JUST A EXAMPLE OF HOW POWERFULL THE USE OF MIDDLEWARE IS!!!
+// a real approach would be verifying the web token
 const authorize = (req, res, next)=>{
+    const {user} = req.query
+    if (user === 'john') {
+        req.user = {name:'john', id:1}
+        next()
+    }
+    else {
+        res.status(401).send("Unauthorized")
+    }
     console.log('authorize');
     next()
 }
